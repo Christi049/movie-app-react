3db0345ba4f4ec80d84f7394fc2d4a7ecf5dcadc
Christi049 <christi.thomas44@gmail.com>
2025-09-10 23:58:25 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({title}) =>{
+  const[hasLiked ,setHasLiked] = useState(false);  
+  return(
+    <div className ="card">
+      <h2>{title}</h2>
+      <button onClick = {() => setHasLiked(!hasLiked)}>
+        {hasLiked? '❤️' : '🤍'}
+      </button>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className = "card-container">
+      <h2>Functional arrow cmponent</h2>
+      <Card title = "Star Wars"/>
+    </div>
+  )
+}
+
+export default App
